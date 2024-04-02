d0b6e136b194c88748e92d3c250ac9a2d0f9c264
RogersKaran <karanjarodgers@gmail.com>
2024-04-02 16:24:07 +0000

    The rest of the webpack task files

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,80 @@
+t path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "[name].bundle.js",
+    chunkFilename: "[id].[chunkhash].js",
+  },
+  performance: {
+    maxAssetSize: 1000000,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "./public"),
+    compress: true,
+    port: 8564,
+    open: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ filename: "index.html" }),
+  ],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+};
