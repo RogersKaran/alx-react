3c63a31817ec78d1951c2cce85a1c06b12176f4c
RogersKaran <karanjarodgers@gmail.com>
2024-04-18 12:55:31 +0000

    All the React State tasks

---
+++ 0x05-react_state/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,68 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const [checked, setChecked] = useState(false);
+  const headerStyle = { backgroundColor: "#deb5b545" };
+  const rowStyle = { backgroundColor: "#f5f5f5ab" };
+  const selected_style = isHeader ? headerStyle : rowStyle;
+
+  const handleCheckChange = (e) => {
+    setChecked(!checked);
+  };
+
+  return (
+    <tr className={isHeader ? css(styles.header) : css(styles.normal)}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan={2}>{textFirstCell}</th>
+        ) : (
+          <>
+            <th>{textFirstCell}</th>
+            <th style={headerStyle}>{textSecondCell}</th>
+          </>
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
+}
+
+const rowStyles = StyleSheet.create({
+   thcenter: {
+    borderBottom: "1px solid gray",
+    margin: 0,
+    padding: 0,
+    textAlign: "center",
+  },
+  th: {
+    borderBottom: "1px solid gray",
+    margin: 0,
+    padding: 0,
+    textAlign: "left",
+  },
+  td: {
+    paddingLeft: 3,
+  },
+  rowChecked: {
+    backgroundColor: "#e6e4e4",
+  },
+});
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+export default CourseListRow;
